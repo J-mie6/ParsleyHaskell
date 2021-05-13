8562d4ffa7cb5af0d8a1c72ed280aa78ba580415
Jamie Willis <j.willis19@imperial.ac.uk>
2021-05-13 11:40:48 +0100

    Cleaned up the precedence file

--- src/ghc/Parsley/Precedence.hs
+++ src/ghc/Parsley/Precedence.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE AllowAmbiguousTypes,
-             MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Parsley.Precedence (module Parsley.Precedence) where
 
 import Prelude hiding                ((<$>), (<*>), pure)
@@ -59,33 +58,3 @@ infixl 5 >+
 infixr 5 +<
 (+<) :: Op a b -> Prec a -> Prec b
 (+<) = flip (>+)
-
-{-data Level a b = InfixL  [Parser (b -> a -> b)] (Defunc (a -> b))
-               | InfixR  [Parser (a -> b -> b)] (Defunc (a -> b))
-               | Prefix  [Parser (b -> b)]      (Defunc (a -> b))
-               | Postfix [Parser (b -> b)]      (Defunc (a -> b))-}
-
-{-class Monolith a b c where
-  infixL  :: [Parser (b -> a -> b)] -> c
-  infixR  :: [Parser (a -> b -> b)] -> c
-  prefix  :: [Parser (b -> b)]      -> c
-  postfix :: [Parser (b -> b)]      -> c
-
-instance x ~ a => Monolith x a (Level a a) where
-  infixL  = flip InfixL ID
-  infixR  = flip InfixR ID
-  prefix  = flip Prefix ID
-  postfix = flip Postfix ID
-
-instance {-# INCOHERENT #-} x ~ (WQ (a -> b) -> Level a b) => Monolith a b x where
-  infixL  ops = InfixL ops . BLACK
-  infixR  ops = InfixR ops . BLACK
-  prefix  ops = Prefix ops . BLACK
-  postfix ops = Postfix ops . BLACK-}
-
-{-data Prec a b where
-  NoLevel :: Prec a a
-  Level :: Level a b -> Prec b c -> Prec a c
-
-monolith :: [Level a a] -> Prec a a
-monolith = foldr Level NoLevel-}
